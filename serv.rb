96bf83cea96285592f0815847dd6a5f23c8e43f1
Jenner LaFave <jenner@prodeditor.dynamic.ucsd.edu>
2012-03-31 05:15:57 -0700

    Added UI and new routes and other cool stuff.

--- serv.rb
+++ serv.rb
@@ -1,16 +1,34 @@
 require 'sinatra'
-require 'micromidi'
+require 'unimidi'
 
-$o = UniMIDI::Output.use :first
+class MIDIPlay
+    attr_accessor :route
+    
+    def initialize(midiroute)
+        @route = midiroute
+    end
 
+    def play(channel, note, velocity, duration)
+        raise ArgumentError if (not channel.to_i.between?(0, 15) \
+                                or not note.to_i.between?(0, 127) \
+                                or not velocity.to_i.between?(0, 127))
+        @route.puts (0x90 | channel), note, velocity
+        sleep(duration)
+        @route.puts (0x80 | channel), note, velocity
+    end
+
+    def self.getFirst()
+        return self.new UniMIDI::Output.open(:first)
+    end
+end
+
+    
 get '/' do
     erb :index
 end
 
-get '/:pitch' do
-    $pitch = params[:pitch]
-    MIDI.using $o do
-        play $pitch, 0.1
-    end
-    redirect '/'
+get '/:channel/:note' do
+    @midi = MIDIPlay.getFirst
+    @midi.play params[:channel].to_i, params[:note].to_i, 64, 0.1
+    puts "played a #{params[:note]} on channel #{params[:channel]} with " + @midi.to_s
 end
